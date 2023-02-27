159b103a4eab159ea0bfc3019fb888bec6808bc8
Anggaalfa21 <105007533+Anggaalfa21@users.noreply.github.com>
2023-02-27 14:52:18 +0700

    Add files via upload

---
+++ Run.py
@@ -0,0 +1,19 @@
+print('kamu nanya?')
+operator = input('pilih Antara +, *, - or /:')
+
+a = int(input('masukan angka pertama: '))
+b = int(input('masukan angka kedua: '))
+
+if operator == '+':
+    print(a, operator, b, '=', a+b)
+
+elif operator == '*':
+    print(a, operator, b, '=', a+b)
+
+elif operator == '-':
+    print(a, operator, b, '=', a-b)
+
+elif operator == '/':
+    print(a, operator, b, '=', a/b)
+
+print('semoga harimu menyenangkan')
